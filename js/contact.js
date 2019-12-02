cac65e48c6a6a4e8b18f6b522490d723e6e6918f
germanquintela <germanquintela96@gmail.com>
2019-12-02 00:47:12 -0300

    contacto

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+function contact() {
+    console.log('Linkedin: https://www.linkedin.com/in/german-quintela-31193b185')
+    console.log('Email: germanquintela96@gmail.com')
+    console.log('GitHub: https://github.com/germanquintela')
+    console.log('Behance: https://www.behance.net/GermanQuintela')
+}
